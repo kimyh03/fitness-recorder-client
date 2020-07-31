915e6f585335b6e1f4f1bb5fd90519a34998bf66
Hoony <59421544+kimyh03@users.noreply.github.com>
2020-07-31 17:47:55 +0900

    Create NoInbodyChart.tsx

---
+++ src/Components/NoInbodyChart.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 40%;
+  background: white;
+  border-radius: 10px;
+  border: ${(props) => props.theme.border};
+  box-shadow: ${(props) => props.theme.boxShadow};
+`;
+
+const Header = styled.div`
+  width: 100%;
+  background: #f7f8fb;
+  border-bottom: 1px solid #e3e6f0;
+  color: #4e73df;
+  font-weight: 700;
+  font-size: 17px;
+  padding: 13px 25px;
+`;
+
+const ChartContainer = styled.div`
+  padding: 25px;
+  width: 100%;
+  text-align: center;
+  opacity: 0.6;
+`;
+
+export default () => (
+  <Container>
+    <Header>Latest inbody data</Header>
+    <ChartContainer>아직 데이터가 없습니다.</ChartContainer>
+  </Container>
+);
